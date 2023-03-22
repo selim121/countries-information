d50b075bda1273c380c68a81d56cf3bca20f58b2
selim121 <selimkhan.bdkhan@gmail.com>
2023-03-22 14:23:09 +0600

    react loaded

---
+++ src/component/Countries/Countries.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Countries.css';
+
+const Countries = () => {
+    return (
+        <div>
+            <h2>Countries Information!!!</h2>
+        </div>
+    );
+};
+
+export default Countries;
+
+// https://restcountries.com/v3.1/all
